63f83410b12c614836de1d2926e9a45ae776f593
Vinaykumar-8 <vinay17022005@gmail.com>
2026-02-23 20:46:31 +0530

    Create cng_container_builder.dart

---
+++ cng_container_builder.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+import 'cng_models.dart';
+
+class CngContainerBuilder {
+  static String buildContainer({
+    required String originalFileName,
+    required List<int> fileBytes,
+    required ClassificationResult classification,
+  }) {
+    final timestamp = DateTime.now().toIso8601String();
+    final extension = originalFileName.split('.').last;
+
+    String payload;
+
+    if (classification.category == FileCategory.programming) {
+      final originalContent = utf8.decode(fileBytes, allowMalformed: true);
+
+      payload = '''
+      $originalContent
+      ''';
+    } else if (classification.category == FileCategory.executable ||
+        classification.category == FileCategory.archive) {
+      payload = base64Encode(fileByte);
+    } else {
+      payload = utf8.decode(fileBytes, allowMalformed: true);
+    }
+
+    return '''
+    [CNG-CONTAINER v1]
+    Original-Name: $originalFileName
+    Original-Extension: $extension
+    Category: ${classification.category.name}
+    Risk-Level: ${classification.risklevel.name}
+    Reason: ${classification.reason}
+    Timestamp: $timestamp
+
+    -----CNG-PAYLOAD-START-----
+    $payload
+    -----CNG-PAYLOAD-END-----
+    ''';
+  }
+}
